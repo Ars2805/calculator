de0433db3e1ec8c934c5e61194b8cae8e53d15f8
Ars <alina.2004.ru@mail.ru>
2025-06-07 16:25:37 +0300

    добавлены кнопки +-, %

--- script.js
+++ script.js
@@ -3,20 +3,52 @@ const buttons = document.querySelectorAll('.calculator__button');
 
 let currentInput = '';
 
+function updateDisplay() {
+  display.value = currentInput;
+}
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const value = button.textContent;
 
-    if (value === '=') {
-      try {
-        currentInput = eval(currentInput).toString();
-      } catch (e) {
-        currentInput = 'Ошибка';
-      }
-    } else {
-      currentInput += value;
+    switch (value) {
+      case 'C':
+        currentInput = '';
+        break;
+      case '⌫':
+        currentInput = currentInput.slice(0, -1);
+        break;
+      case '%':
+        if (currentInput) {
+          try {
+            currentInput = (eval(currentInput) / 100).toString();
+          } catch (e) {
+            currentInput = 'Ошибка';
+          }
+        }
+        break;
+      case '+/-':
+        if (currentInput) {
+          try {
+            let val = eval(currentInput);
+            currentInput = (-val).toString();
+          } catch (e) {
+            currentInput = 'Ошибка';
+          }
+        }
+        break;
+      case '=':
+        try {
+          currentInput = eval(currentInput).toString();
+        } catch (e) {
+          currentInput = 'Ошибка';
+        }
+        break;
+      default:
+        currentInput += value;
+        break;
     }
 
-    display.value = currentInput;
+    updateDisplay();
   });
 });
